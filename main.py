5d840f29fb10376adfceaddb62c62f424aa4c2dd
chiefchewie <69645299+chiefchewie@users.noreply.github.com>
2022-08-13 11:17:43 -0400

    v2 - use new api and stuff

--- main.py
+++ main.py
@@ -1,171 +1,79 @@
-import json
 import os
+from datetime import datetime
 
 import pandas as pd
-import requests
-import shopify
 from dotenv import load_dotenv
-from requests.models import HTTPError
+from tqdm import tqdm
+
+import shopify
+from tracker import get_tracking
 
 
-def get_tracking(*args: str):
-    """Returns the latest available shipping info from Aftership
-
-    Args:
-      args: one or more tracking numbers
-
-    Returns:
-      a dictionary in the form {tracking_id: [last_update_time, checkpoint_msg, raw_location (if possible)]}
-
-    Raises:
-      HTTPError: If request to Aftership API failed
-    """
-    aftership_api = "https://track.aftership.com/api/v2/direct-trackings/batch"
-    payload = {"direct_trackings": []}
-    head = {
-        "Host": "track.aftership.com",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "en-CA,en-US;q=0.7,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Content-Type": "application/json",
-        "b3": "69b0488be4bf54271e7384501c316882-2c9991fdcae410c8-0",
-        "Content-Length": "97",
-        "Origin": "https://track.aftership.com",
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0",
-        "TE": "Trailers"
-    }
-    for id in args:
-        data = {
-            "tracking_number": id,
-            "additional_fields": {},
-            # "slug": "yunexpress" # - this line may not be necessary
-        }
-        payload["direct_trackings"].append(data)
-
-    response = requests.post(url=aftership_api, json=payload, headers=head)
-    if response.status_code != 201:
-        raise HTTPError(
-            f"Error for post request: ids={args}, response={response.status_code}")
-
-    result_dict = json.loads(response.text)
-    return_dict = {}
-    for tracking in result_dict["data"]["direct_trackings"]:
-        num = tracking["tracking_number"]
-        if tracking["tracking"]["checkpoints"]:
-            latest_msg = [
-                tracking["tracking"]["checkpoints"][-1]["date_time"],
-                tracking["tracking"]["checkpoints"][-1]["message"]
-            ]
-            if tracking["tracking"]["checkpoints"][-1]["address"]:
-                latest_msg.append(
-                    tracking["tracking"]["checkpoints"][-1]["address"]["raw_location"]
-                )
-        else:
-            latest_msg = "N/A"
-        return_dict[num] = latest_msg
-    return return_dict
+def get_all_orders():
+    latest_order = 0
+    orders = []
+    while True:
+        query = shopify.Order.find(since_id=latest_order, limit=30)
+        orders += query
+        latest_order = orders[-1].id
+        if len(query) < 250:
+            break
+    return orders
 
 
 if __name__ == "__main__":
+    start_time = datetime.now()
+
     load_dotenv()
-
     API_KEY = os.getenv("API_KEY")
-    PASSWORD = os.getenv("PASSWORD")
+    APP_PASSWORD = os.getenv("APP_PASSWORD")
     SHOP_NAME = os.getenv("SHOP_NAME")
+    API_VERSION = "2022-07"
 
-    API_VERSION = "2021-10"
-    SHOP_URL = f"https://{API_KEY}:{PASSWORD}@{SHOP_NAME}.myshopify.com/admin"
+    shop_url = f"https://{API_KEY}:{APP_PASSWORD}@{SHOP_NAME}.myshopify.com/admin"
+    session = shopify.Session(shop_url, API_VERSION, APP_PASSWORD)
 
-    # --BEGIN DOING THINGS--
-    session = shopify.Session(SHOP_URL, API_VERSION, PASSWORD)
     shopify.ShopifyResource.activate_session(session)
 
-    # keys to keep while reading orders
-    useful_keys: list[str] = [
+    keys = (
         "created_at",
-        "customer",
         "email",
         "fulfillments",
-        "fulfillment_status",
-        "id",
-        "name"
-    ]
+        "name",
+    )
 
-    # get all orders
-    all_orders = shopify.Order.find(limit=20)
-
-    # list of all parsed orders
     order_results = []
 
-    # mapping of tracking num to index
-    tracking_to_list_index: dict[str, int] = {}
+    all_orders = get_all_orders()
+    for o in tqdm(all_orders):
+        order = {key: o.attributes[key] for key in keys}
 
-    # set of all tracking numbers
-    all_tracking_numbers = set()
+        order["tracking_numbers"] = []
+        order["tracking_urls"] = []
+        order["last_update_time"] = []
+        order["shipping_status"] = []
+        order["events"] = []
 
-    for i, order in enumerate(all_orders):
+        if order["fulfillments"]:
+            for fulfillment in order["fulfillments"]:
+                for track_num in fulfillment.tracking_numbers:
+                    status = get_tracking(track_num)
 
-        # store useful attributes
-        attrs = {k: order.attributes[k] for k in useful_keys}
-        attrs["customer_name"] = order.attributes["billing_address"].attributes["name"]
+                    order["tracking_numbers"].append(track_num)
+                    order["last_update_time"].append(status["last_update_time"])
+                    order["shipping_status"].append(status["shipping_status"])
+                    order["events"] += status["events"]
 
-        attrs["tracking_company"] = ""
-        attrs["tracking_number"] = ""
-        attrs["tracking_urls"] = ""
-        attrs["shipping_status"] = ""
-        attrs["updated_at"] = ""
-        # note - only works with one fulfillment and one tracking number
-        if attrs["fulfillments"]:
-            # TODO this returns an array - this code only deals with the first element
-            current_fulfillment = attrs["fulfillments"][0]
+                for track_url in fulfillment.tracking_urls:
+                    order["tracking_urls"].append(track_url)
 
-            # get tracking company name
-            attrs["tracking_company"] = current_fulfillment.attributes["tracking_company"]
+        order_results.append(order)
 
-            # TODO these return arrays but this only deals with the first one
-            attrs["tracking_number"] = current_fulfillment.attributes["tracking_numbers"][0]
-            attrs["tracking_urls"] = current_fulfillment.attributes["tracking_urls"][0]
-
-            # store mappings - used to insert tracking information back into the indexed list
-            tracking_to_list_index[attrs["tracking_number"]] = i
-
-            all_tracking_numbers.add(attrs["tracking_number"])
-        order_results.append(attrs)
-
-    while all_tracking_numbers:
-        # pop the top 50 or whats left into a list
-        top50 = [all_tracking_numbers.pop()
-                 for _ in range(min(50, len(all_tracking_numbers)))]
-
-        # get tracking for these tracking numbers
-        res = get_tracking(*top50)
-        for tracking_num, status in res.items():
-            # find the index of each tracking number
-            # allowing us to insert the status message into the relevant field
-            idx = tracking_to_list_index[tracking_num]
-
-            # msg + location
-            order_results[idx]["shipping_status"] = status[1:]
-
-            # updated_time is a separate field
-            order_results[idx]["updated_at"] = status[0]
-
-    # convert list to dataframe
     df = pd.DataFrame(order_results)
+    df.drop("fulfillments", axis=1, inplace=True)
+    df.to_excel("orders.xlsx")
 
-    # column names - change as needed
-    cols = ['name', 'created_at', 'updated_at', 'customer_name', 'email',  'tracking_company',
-            'tracking_number', 'tracking_urls', 'shipping_status']
-    df = df[cols]
-
-    # output directory - where to store the Excel file
-    directory = "./output"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    df.to_excel(f"output/orders.xlsx")
-
-    # --FINISH DOING THINGS--
     shopify.ShopifyResource.clear_session()
+
+    end_time = datetime.now()
+    print("Duration: {}".format(end_time - start_time))
