15e722ab7c1fa1a8913094a2a134502aea87ea4f
chiefchewie <69645299+chiefchewie@users.noreply.github.com>
2022-08-13 11:31:45 -0400

    use httpx.client

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 
+import httpx
 import pandas as pd
 from dotenv import load_dotenv
 from tqdm import tqdm
@@ -13,7 +14,7 @@ def get_all_orders():
     latest_order = 0
     orders = []
     while True:
-        query = shopify.Order.find(since_id=latest_order, limit=30)
+        query = shopify.Order.find(since_id=latest_order, limit=250)
         orders += query
         latest_order = orders[-1].id
         if len(query) < 250:
@@ -31,9 +32,9 @@ if __name__ == "__main__":
     API_VERSION = "2022-07"
 
     shop_url = f"https://{API_KEY}:{APP_PASSWORD}@{SHOP_NAME}.myshopify.com/admin"
-    session = shopify.Session(shop_url, API_VERSION, APP_PASSWORD)
 
-    shopify.ShopifyResource.activate_session(session)
+    with shopify.Session.temp(shop_url, API_VERSION, APP_PASSWORD):
+        all_orders = get_all_orders()
 
     keys = (
         "created_at",
@@ -44,36 +45,34 @@ if __name__ == "__main__":
 
     order_results = []
 
-    all_orders = get_all_orders()
-    for o in tqdm(all_orders):
-        order = {key: o.attributes[key] for key in keys}
+    with httpx.Client() as client:
+        for o in tqdm(all_orders):
+            order = {key: o.attributes[key] for key in keys}
 
-        order["tracking_numbers"] = []
-        order["tracking_urls"] = []
-        order["last_update_time"] = []
-        order["shipping_status"] = []
-        order["events"] = []
+            order["tracking_numbers"] = []
+            order["tracking_urls"] = []
+            order["last_update_time"] = []
+            order["shipping_status"] = []
+            order["events"] = []
 
-        if order["fulfillments"]:
-            for fulfillment in order["fulfillments"]:
-                for track_num in fulfillment.tracking_numbers:
-                    status = get_tracking(track_num)
+            if order["fulfillments"]:
+                for fulfillment in order["fulfillments"]:
+                    for track_num in fulfillment.tracking_numbers:
+                        status = get_tracking(client, track_num)
 
-                    order["tracking_numbers"].append(track_num)
-                    order["last_update_time"].append(status["last_update_time"])
-                    order["shipping_status"].append(status["shipping_status"])
-                    order["events"] += status["events"]
+                        order["tracking_numbers"].append(track_num)
+                        order["last_update_time"].append(status["last_update_time"])
+                        order["shipping_status"].append(status["shipping_status"])
+                        order["events"] += status["events"]
 
-                for track_url in fulfillment.tracking_urls:
-                    order["tracking_urls"].append(track_url)
+                    for track_url in fulfillment.tracking_urls:
+                        order["tracking_urls"].append(track_url)
 
-        order_results.append(order)
+            order_results.append(order)
 
     df = pd.DataFrame(order_results)
     df.drop("fulfillments", axis=1, inplace=True)
     df.to_excel("orders.xlsx")
-
-    shopify.ShopifyResource.clear_session()
 
     end_time = datetime.now()
     print("Duration: {}".format(end_time - start_time))
--- tracker.py
+++ tracker.py
@@ -1,40 +1,23 @@
-from pprint import pprint
-import sys
-
 import httpx
 
 
-def get_tracking(tracking_number: str):
-    base_url = f"https://api.ship24.com/api/parcels/{tracking_number}"
+def get_tracking(client: httpx.Client, tracking_number: str):
+    api_url = f"https://api.ship24.com/api/parcels/{tracking_number}"
     headers = {
-        # "accept": "application/json, text/plain, */*",
-        # "accept-encoding": "gzip, deflate, br",
-        # "accept-language": "en-CA,en;q=0.9",
-        # "content-type": "application/json",
         "origin": "https://www.ship24.com",
         "referer": "https://www.ship24.com/",
-        # "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"',
-        # "sec-ch-ua-mobile": "?0",
-        # "sec-ch-ua-platform": "Windows",
-        # "sec-fetch-dest": "empty",
-        # "sec-fetch-mode": "cors",
-        # "sec-fetch-site": "same-site",
-        # "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54",
         "x-ship24-token": "225,150,128,44,49,54,54,48,51,57,55,51,51,55,51,53,51,44,225,150,130",
     }
-    try:
-        response = httpx.post(headers=headers, url=base_url)
-    except httpx.ReadTimeout as e:
-        print(e)
-        return
 
+    response = client.post(headers=headers, url=api_url, timeout=None)
     info = response.json()
 
     last_update_time = info["data"]["events"][0]["datetime"]
-    events = []
-    for e in info["data"]["events"]:
-        events.append(e["status"])
+
+    events = [e["status"] for e in info["data"]["events"]]
+
     shipping_status = info["data"]["dispatch_code"]["desc"]
+
     return {
         "last_update_time": last_update_time,
         "events": events,
@@ -42,6 +25,7 @@ def get_tracking(tracking_number: str):
     }
 
 
-# YT2221521272091049
-# YT2222421272086922
-get_tracking("YT2221521272091049")
+if __name__ == "__main__":
+    with httpx.Client() as client:
+        get_tracking(client, "YT2221521272091049")
+        get_tracking(client, "YT2222421272086922")
