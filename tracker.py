7844eeba5521a29a42983524d495561222c77f9b
chiefchewie <69645299+chiefchewie@users.noreply.github.com>
2022-08-14 15:10:13 -0400

    fix request scheme

--- tracker.py
+++ tracker.py
@@ -1,15 +1,37 @@
 import httpx
-
-
 def get_tracking(client: httpx.Client, tracking_number: str):
-    api_url = f"https://api.ship24.com/api/parcels/{tracking_number}"
-    headers = {
-        "origin": "https://www.ship24.com",
-        "referer": "https://www.ship24.com/",
-        "x-ship24-token": "225,150,128,44,49,54,54,48,51,57,55,51,51,55,51,53,51,44,225,150,130",
+
+    url = f"https://api.ship24.com/api/parcels/{tracking_number}?lang=en"
+
+    json_payload = {
+        "userAgent": "",
+        "os": "Windows",
+        "browser": "MS-Edge-Chromium",
+        "device": "Unknown",
+        "os_version": "windows-10",
+        "browser_version": "104.0.1293.54",
+        "uL": "en-CA",
     }
 
-    response = client.post(headers=headers, url=api_url, timeout=None)
+    headers = {
+        "authority": "api.ship24.com",
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-CA,en;q=0.9",
+        "content-type": "application/json",
+        "origin": "https://www.ship24.com",
+        "referer": "https://www.ship24.com/",
+        "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54",
+        "x-ship24-token": "225,150,128,44,49,54,54,48,53,48,51,56,56,51,57,54,49,44,225,150,130",
+    }
+
+    response = client.post(url=url, headers=headers, json=json_payload)
+
     info = response.json()
 
     last_update_time = info["data"]["events"][0]["datetime"]
